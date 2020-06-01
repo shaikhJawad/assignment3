cb15b2cba236c0f9506f5c6d28161e2495b2ad3b
Infosys <Infosys@Infosys-PC>
2020-06-02 01:49:03 +0300

    simple value issue

--- assignment3/src/test/java/assignment3/StudentIntegrationTest.java
+++ assignment3/src/test/java/assignment3/StudentIntegrationTest.java
@@ -28,10 +28,10 @@ public class StudentIntegrationTest {
 		Student s = new Student();
 		ageCalculation a = new ageCalculation();
 		
-		SimpleDateFormat sd = new SimpleDateFormat();
+		SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd");
 		Date dob;
 		try {
-			dob = sd.parse("2020-01-01");
+			dob = sd.parse("1990-01-01");
 			int age = a.calcAge(dob);
 			s.setAge(age);
 			Assert.assertEquals(30, s.getAge());
